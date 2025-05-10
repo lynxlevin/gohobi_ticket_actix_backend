e3c24aaf79959db0922590859fe08a2a5a299335
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-05-10 10:49:45 +0900

    feat (general): Better get_settings method

--- src/general/settings/mod.rs
+++ src/general/settings/mod.rs
@@ -1,45 +1,72 @@
+use std::env::{self, VarError};
+
+use types::{
+    ApplicationSettings, DatabaseSettings, Environment, RedisSettings, SecretSettings, Settings,
+};
+
 pub mod types;
 
-pub fn get_settings(env_name: &str) -> Result<types::Settings, String> {
-    dotenvy::from_filename(env_name).ok();
-    let base_path = match std::env::current_dir() {
-        Ok(path) => path,
-        Err(e) => {
-            return Err(format!(
-                "Failed to determine the current directory: {}",
-                e.to_string()
-            ))
-        }
-    };
-    let settings_directory = base_path.join("src/general/settings/yaml");
+pub fn get_settings(env_file_name: &str) -> Result<Settings, String> {
+    dotenvy::from_filename(env_file_name)
+        .map_err(|e| format!("Failed to fetch env file: {}", e.to_string()))?;
 
-    let environment: types::Environment = match std::env::var("APP_ENVIRONMENT")
-        .unwrap_or_else(|_| "development".into())
-        .try_into()
+    match Environment::try_from(env::var("APP_ENVIRONMENT").unwrap_or_else(|_| "production".into()))
     {
-        Ok(env) => env,
+        Ok(env) => match env {
+            Environment::Testing => get_development_settings(),
+            Environment::Development => get_development_settings(),
+            Environment::Production => get_production_settings(),
+        },
         Err(e) => return Err(format!("Failed to parse APP_ENVIRONMENT: {}", e)),
-    };
-    let environment_filename = format!("{}.yaml", environment.as_str());
-    let settings = match config::Config::builder()
-        .add_source(config::File::from(settings_directory.join("base.yaml")))
-        .add_source(config::File::from(
-            settings_directory.join(environment_filename),
-        ))
-        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
-        // E.g. 'APP_APPLICATION__PORT=5001' would set 'Settings.application.port'
-        .add_source(
-            config::Environment::with_prefix("APP")
-                .prefix_separator("_")
-                .separator("__"),
-        )
-        .build()
-    {
-        Ok(settings) => settings,
-        Err(e) => return Err(format!("Failed to build config: {}", e.to_string())),
-    };
+    }
+}
 
-    settings
-        .try_deserialize::<types::Settings>()
-        .map_err(|e| format!("Failed to read settings: {}", e.to_string()))
+fn get_development_settings() -> Result<Settings, String> {
+    let b = Settings::base_settings();
+    merge_env(Settings {
+        application: ApplicationSettings {
+            host: "127.0.0.1".to_string(),
+            protocol: "http".to_string(),
+            ..b.application
+        },
+        debug: true,
+        ..b
+    })
+    .map_err(|e| format!("Failed to get env variables: {}", e.to_string()))
+}
+
+fn get_production_settings() -> Result<Settings, String> {
+    let b = Settings::base_settings();
+    merge_env(Settings {
+        application: ApplicationSettings {
+            host: "0.0.0.0".to_string(),
+            protocol: "https".to_string(),
+            ..b.application
+        },
+        debug: false,
+        ..b
+    })
+    .map_err(|e| format!("Failed to get env variables: {}", e.to_string()))
+}
+
+fn merge_env(s: Settings) -> Result<Settings, VarError> {
+    Ok(Settings {
+        database: DatabaseSettings {
+            url: env::var("DATABASE_URL")?,
+        },
+        debug: match env::var("APP_DEBUG") {
+            Ok(debug) => &debug == "true",
+            Err(_) => s.debug,
+        },
+        redis: RedisSettings {
+            url: env::var("REDIS_URL")?,
+            ..s.redis
+        },
+        secret: SecretSettings {
+            secret_key: env::var("APP_SECRET__SECRET_KEY")?,
+            hmac_secret: env::var("APP_SECRET__HMAC_SECRET")?,
+            ..s.secret
+        },
+        ..s
+    })
 }
--- src/general/settings/types.rs
+++ src/general/settings/types.rs
@@ -1,39 +1,62 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Default, Debug)]
 pub struct Settings {
     pub application: ApplicationSettings,
     pub database: DatabaseSettings,
     pub debug: bool,
     pub redis: RedisSettings,
-    pub secret: Secret,
+    pub secret: SecretSettings,
 }
 
-#[derive(Deserialize, Clone)]
+impl Settings {
+    pub fn base_settings() -> Self {
+        Self {
+            application: ApplicationSettings {
+                port: 5000,
+                ..Default::default()
+            },
+            redis: RedisSettings {
+                pool_max_open: 16,
+                pool_max_idle: 8,
+                pool_timeout_seconds: 1,
+                pool_expire_seconds: 60,
+                ..Default::default()
+            },
+            secret: SecretSettings {
+                token_expiration: 30,
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Deserialize, Clone, Default, Debug)]
 pub struct ApplicationSettings {
-    pub port: u16,
+    pub port: u32,
     pub host: String,
     pub protocol: String,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Default, Debug)]
 pub struct DatabaseSettings {
     pub url: String,
 }
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Deserialize, Clone, Debug, Default)]
 pub struct RedisSettings {
     pub url: String,
-    pub pool_max_open: u64,
-    pub pool_max_idle: u64,
-    pub pool_timeout_seconds: u64,
-    pub pool_expire_seconds: u64,
+    pub pool_max_open: u32,
+    pub pool_max_idle: u32,
+    pub pool_timeout_seconds: u32,
+    pub pool_expire_seconds: u32,
 }
 
-#[derive(Deserialize, Clone)]
-pub struct Secret {
+#[derive(Deserialize, Clone, Default, Debug)]
+pub struct SecretSettings {
     pub secret_key: String,
-    pub token_expiration: i64,
+    pub token_expiration: u32,
     pub hmac_secret: String,
 }
 
@@ -41,16 +64,6 @@ pub enum Environment {
     Testing,
     Development,
     Production,
-}
-
-impl Environment {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Environment::Testing => "testing",
-            Environment::Development => "development",
-            Environment::Production => "production",
-        }
-    }
 }
 
 impl TryFrom<String> for Environment {
