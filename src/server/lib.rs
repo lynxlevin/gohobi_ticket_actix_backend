f3491aaf03de65b2a2b4d4c90ffe9e76c1f25665
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-05-14 15:02:44 +0900

    feat: Move part of the app creation logic to server crate

---
+++ src/server/lib.rs
@@ -0,0 +1,39 @@
+use actix_session::{
+    config::{PersistentSession, SessionMiddlewareBuilder},
+    storage::RedisSessionStore,
+};
+use actix_web::{cookie, web::scope, Scope};
+use general::settings::types::Settings;
+use user::auth_routes;
+
+pub fn setup_session_middleware_builder(
+    builder: SessionMiddlewareBuilder<RedisSessionStore>,
+    settings: &Settings,
+) -> SessionMiddlewareBuilder<RedisSessionStore> {
+    if settings.debug {
+        builder
+            .session_lifecycle(
+                PersistentSession::default().session_ttl(cookie::time::Duration::days(7)),
+            )
+            .cookie_name("sessionId".to_string())
+            .cookie_same_site(cookie::SameSite::None)
+            .cookie_secure(false)
+    } else {
+        builder
+            .session_lifecycle(
+                PersistentSession::default().session_ttl(cookie::time::Duration::days(7)),
+            )
+            .cookie_name("sessionId".to_string())
+    }
+}
+
+pub fn get_routes() -> Scope {
+    scope("/api").configure(auth_routes) // .configure(routes::ambition_routes)
+                                         // .configure(routes::desired_state_routes)
+                                         // .configure(routes::action_routes)
+                                         // .configure(routes::mindset_routes)
+                                         // .configure(routes::reading_note_routes)
+                                         // .configure(routes::tag_routes)
+                                         // .configure(routes::action_track_routes)
+                                         // .configure(routes::diary_routes),
+}
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,12 +1,12 @@
 use actix_http::{encoding::Encoder, Request};
-use actix_session::{config::PersistentSession, storage::RedisSessionStore, SessionMiddleware};
+use actix_session::{storage::RedisSessionStore, SessionMiddleware};
 use actix_web::{
     body::{BoxBody, EitherBody},
     cookie,
     dev::{Service, ServiceResponse},
     middleware::Compress,
     test,
-    web::{scope, Data},
+    web::Data,
     App, Error,
 };
 use general::{
@@ -15,7 +15,7 @@ use general::{
     settings::{get_test_settings, types::Settings},
 };
 use sea_orm::{DbConn, DbErr};
-use user::auth_routes;
+use server::{get_routes, setup_session_middleware_builder};
 
 pub struct Connections<
     S: Service<Request, Response = ServiceResponse<EitherBody<Encoder<BoxBody>>>, Error = Error>,
@@ -38,64 +38,28 @@ pub async fn init_app() -> Result<
     let redis_pool = init_redis_pool(&settings)
         .await
         .expect("Error on getting Redis pool.");
-    let session_middleware = init_session_middleware(&settings)
+
+    let secret_key = cookie::Key::from(settings.secret.hmac_secret.as_bytes());
+    let redis_store = RedisSessionStore::new(&settings.redis.url)
         .await
-        .expect("Error on initializing session middleware.");
+        .expect("Error on getting RedisSessionStore");
+
     let app = test::init_service(
         // MYMEMO: This should be completely the same as in startup.rs
         App::new()
             .wrap(Compress::default())
-            .wrap(session_middleware)
-            .service(
-                scope("/api").configure(auth_routes), // .configure(routes::ambition_routes)
-                                                      // .configure(routes::desired_state_routes)
-                                                      // .configure(routes::action_routes)
-                                                      // .configure(routes::mindset_routes)
-                                                      // .configure(routes::reading_note_routes)
-                                                      // .configure(routes::tag_routes)
-                                                      // .configure(routes::action_track_routes)
-                                                      // .configure(routes::diary_routes),
+            .wrap(
+                setup_session_middleware_builder(
+                    SessionMiddleware::builder(redis_store.clone(), secret_key.clone()),
+                    &settings,
+                )
+                .build(),
             )
+            .service(get_routes())
             .app_data(Data::new(db.clone()))
             .app_data(Data::new(redis_pool.clone()))
             .app_data(Data::new(settings.clone())),
     )
     .await;
     Ok(Connections { app, db, settings })
-}
-
-async fn init_session_middleware(
-    settings: &Settings,
-) -> Result<SessionMiddleware<RedisSessionStore>, String> {
-    let secret_key = cookie::Key::from(settings.secret.hmac_secret.as_bytes());
-    let redis_store = match RedisSessionStore::new(&settings.redis.url).await {
-        Ok(store) => store,
-        Err(e) => {
-            return Err(format!(
-                "Failed to init redis session store: {}",
-                e.to_string()
-            ))
-        }
-    };
-    if settings.debug {
-        Ok(
-            SessionMiddleware::builder(redis_store.clone(), secret_key.clone())
-                .session_lifecycle(
-                    PersistentSession::default().session_ttl(cookie::time::Duration::days(7)),
-                )
-                .cookie_name("sessionId".to_string())
-                .cookie_same_site(cookie::SameSite::None)
-                .cookie_secure(false)
-                .build(),
-        )
-    } else {
-        Ok(
-            SessionMiddleware::builder(redis_store.clone(), secret_key.clone())
-                .session_lifecycle(
-                    PersistentSession::default().session_ttl(cookie::time::Duration::days(7)),
-                )
-                .cookie_name("sessionId".to_string())
-                .build(),
-        )
-    }
 }
