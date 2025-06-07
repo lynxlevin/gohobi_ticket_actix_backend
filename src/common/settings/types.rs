1f80614e262742b7681b3ed65e558ae0921dfcdc
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-06-07 14:38:27 +0900

    choir: Change application port to avoid conflict with other apps

--- src/common/settings/types.rs
+++ src/common/settings/types.rs
@@ -13,7 +13,7 @@ impl Settings {
     pub fn base_settings() -> Self {
         Self {
             application: ApplicationSettings {
-                port: 5000,
+                port: 8000,
                 ..Default::default()
             },
             ..Default::default()
