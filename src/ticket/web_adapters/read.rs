25a173ef74803f5c2da614699dbe54b7e1b6c767
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-05-14 11:02:10 +0900

    feat: TicketService.mark_ticket_read

--- src/ticket/web_adapters/read.rs
+++ src/ticket/web_adapters/read.rs
@@ -3,16 +3,16 @@ use actix_web::{
     web::{Data, Path, ReqData},
     HttpResponse,
 };
-use common::errors::{
-    error_responses::{response_401, response_403, response_404, response_500},
-    use_case_errors::UseCaseError,
-};
-use db_adapters::ticket::{TicketMutation, TicketQuery};
+use common::errors::error_responses::{response_401, response_403, response_404, response_500};
+use db_adapters::ticket_service::TicketService;
 use entities::users_user;
 use sea_orm::DbConn;
 use serde::{Deserialize, Serialize};
 
-use crate::{use_cases::read::read_ticket, UpsertTicketResponse};
+use crate::{
+    use_cases::read::{read_ticket, ReadTicketError},
+    UpsertTicketResponse,
+};
 
 #[derive(Deserialize, Serialize, Debug)]
 struct PathParam {
@@ -27,21 +27,22 @@ async fn read_ticket_endpoint(
 ) -> HttpResponse {
     match user {
         Some(user) => {
-            let ticket_query = TicketQuery::init_query(&db);
-            let ticket_mutation = TicketMutation { db: &db };
+            let ticket_service = TicketService::init(&db);
             match read_ticket(
                 user.into_inner(),
-                ticket_query,
-                ticket_mutation,
                 path_param.into_inner().ticket_id,
+                ticket_service,
             )
             .await
             {
                 Ok(ticket) => HttpResponse::Ok().json(UpsertTicketResponse { ticket }),
                 Err(e) => match e {
-                    UseCaseError::Forbidden => response_403("You cannot read this ticket."),
-                    UseCaseError::NotFound => response_404("Ticket not found."),
-                    _ => response_500(),
+                    ReadTicketError::Forbidden(message) => response_403(&message),
+                    ReadTicketError::NotFound(message) => response_404(&message),
+                    ReadTicketError::InternalServerError(message) => {
+                        dbg!(message);
+                        response_500()
+                    }
                 },
             }
         }
