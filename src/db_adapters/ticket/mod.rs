e6f438faa095cb85356a8490dfacf787748541bf
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-05-21 16:12:54 +0900

    chore: remove TicketQuery

--- src/db_adapters/ticket/mod.rs
+++ src/db_adapters/ticket/mod.rs
@@ -1,8 +1,6 @@
-mod ticket_query;
 pub mod types;
 mod wish_mutation;
 mod wish_query;
 
-pub use ticket_query::{Order, TicketQuery};
 pub use wish_mutation::WishMutation;
-pub use wish_query::WishQuery;
+pub use wish_query::{Order, WishQuery};
