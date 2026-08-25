75fc0f09f553736588f42d5a561020ff4d21da4d
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-08-25 21:28:18 +0900

    refactor: use the new Db struct in factory

--- src/common/factory/user.rs
+++ src/common/factory/user.rs
@@ -1,13 +1,15 @@
 use entities::users_user;
-use sea_orm::{DbConn, DbErr, EntityTrait, Set};
+use sea_orm::{DbErr, EntityTrait, Set};
 use uuid::Uuid;
 
-pub async fn get_users(db: &DbConn) -> Result<[users_user::Model; 3], DbErr> {
+use crate::db::Db;
+
+pub async fn get_users(db: &Db) -> Result<[users_user::Model; 3], DbErr> {
     let user_0 = user().username("user_0");
     let user_1 = user().username("user_1");
     let user_2 = user().username("user_2");
     let users = users_user::Entity::insert_many([user_0, user_1, user_2])
-        .exec_with_returning(db)
+        .exec_with_returning(&db.db)
         .await?;
     Ok(users.try_into().unwrap())
 }
